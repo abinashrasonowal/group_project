c73367e0c442b7a4c9f436a879e7ae322dbdbdf2
Abinash <abinashsonowal2002@gmail.com>
2023-03-18 18:54:28 +0530

    react template

---
+++ app/src/navbar.js
@@ -0,0 +1,12 @@
+
+function navbar() {
+    return (
+        <div >
+            <p>
+                Edit <code>src/App.js</code> and save to reload.
+            </p>
+        </div>
+    );
+}
+
+export default navbar;
